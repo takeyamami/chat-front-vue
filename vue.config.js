c06fb703a670c544451e09d1b9b42b50869246bf
takeyamami <task8705@yahoo.co.jp>
2021-06-22 22:09:38 +0900

    add: APIを呼び出す実装を追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  
+  outputDir: '../backend/chat/src/main/resources/static',
+  indexPath: '/index.html',
+  devServer: {
+       host: 'localhost',
+       port: 8080
+   }
 }
