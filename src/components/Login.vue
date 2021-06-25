6cb3bcb42865c888854ba82a75c7179de8c4a01e
takeyamami <task8705@yahoo.co.jp>
2021-06-25 21:06:29 +0900

    update: v-cardを使って調整

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,64 +1,48 @@
 <template>
+  <v-card width="400px" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">ログイン</h1>
+    </v-card-title>
     <v-form>
-    <v-container>
-      <div 
-        class="text-h5 d-flex align-center justify-center pa-10 mx-auto"
-        max-width="550"
-        min-height="76"
-        outlined
-      >
-        ログイン
-      </div>
-      <v-row 
-        justify="center"
-        align-content="center"
-        >
-        <v-col
-          cols="12"
-          sm="6"
-        >
-         <v-text-field
-            v-model="email"
-            :rules="[rules.required, rules.email]"
-            label="メールアドレス"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <v-text-field
-            v-model="password"
-            :append-icon="mdi-eye-off"
-            :rules="[rules.required, rules.min]"
-            :type="show1 ? 'text' : 'password'"
-            name="input-10-1"
-            label="パスワード"
-            hint="At least 8 characters"
-            counter
-            @click:append="show1 = !show1"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-      <v-btn
+      <v-card-text>
+        <v-text-field
+        v-model="email"
+        :rules="[rules.required, rules.email]"
+        label="メールアドレス"
+        prepend-icon="mdi-account-circle"
+        ></v-text-field>
+        <v-text-field
+        v-model="password"
+        :rules="[rules.required, rules.min]"
+        :type="showPassword ? 'text' : 'password'"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        name="input-10-1"
+        label="パスワード"
+        hint="At least 6 characters"
+        counter
+        @click:append="showPassword = !showPassword"
+        prepend-icon="mdi-lock"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
         color="primary"
         elevation="4"
         raised
         large
         rounded
-      >ログイン</v-btn>
-      </v-row>
-    </v-container>
-  </v-form>
+        >ログイン</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
 </template>
 
 <script>
   export default {
     data () {
       return {
+        showPassword: false,
+        password: 'Password',
         rules: {
           required: value => !!value || 'Required.',
           min: v => v.length >= 6 || 'Min 6 characters',
@@ -68,7 +52,7 @@
             const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             return pattern.test(value) || 'Invalid e-mail.'
           },
-        }
+        },
       }
     },
   }
