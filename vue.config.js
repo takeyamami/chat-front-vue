e5bcf2361ce6aa5356c0f7559288250930b69137
takeyamami <task8705@yahoo.co.jp>
2021-07-05 20:31:55 +0900

    update: 未使用の設定を削除

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ],
-  
-  outputDir: '../backend/chat/src/main/resources/static',
-  indexPath: '/index.html',
-  devServer: {
-       host: 'localhost',
-       port: 8080
-   }
+  ]
 }
