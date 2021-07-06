1b5d3813203e48ed5ec5a7de93ed14727f608dd1
takeyamami <task8705@yahoo.co.jp>
2021-07-06 22:07:47 +0900

    update: デザインを少し調整

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,56 +3,58 @@
 
 <template>
   <div>
-    <v-navigation-drawer permanent
-        fixed>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            なまえなまえ
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            xxxx@gmail.com
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.link"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <v-divider></v-divider>
-
-    </v-navigation-drawer>
     <v-main>
-      <div style="margin-left:300px">
-      <v-container>
-        <v-card
-          v-for="talk in talks"
-          :key="talk.tid"
+      <v-container ml-0 pl-0>
+        <v-row>
+        <v-col 
+          cols="3"
         >
-          <div class="talk">
-              <div class="message">
-                  <p>{{talk.message}}</p>
-              </div>
-          </div>
-        </v-card>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                なまえなまえ
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                xxxx@gmail.com
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list
+            dense
+            nav
+          >
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              :to="item.link"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-divider></v-divider>
+        </v-col>
+        <v-col cols="7">
+          <v-card
+            class="mt-5"
+            max-width="344"
+            v-for="talk in talks"
+            :key="talk.tid"
+          >
+            <v-card-text>
+              <p style="white-space:pre-wrap; word-wrap:break-word;">{{talk.message}}</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        </v-row>
       </v-container>
-      </div>
     </v-main>
   </div>
 </template>
@@ -80,7 +82,6 @@
         this.axios
           .get('http://localhost:8080/talk/list/1')
           .then(response => {
-            console.log(response)
             this.talks = response.data
           })
           .catch((e) => {
